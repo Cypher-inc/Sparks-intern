2b130c1adf6ca3aaac2cc4987827a60254983de7
Cypher-inc <victorynorden@gmail.com>
2022-01-03 21:33:23 +0530

    sparks1

---
+++ Kris_Covid19/corona.js
@@ -0,0 +1,9 @@
+var settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://covid-19-data.p.rapidapi.com/docs.json",
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "covid-19-data.p.rapidapi.com",
+		"x-rapidapi-key": "e7b3cef082msh670ae7305ed1bd1p1b443ejsn57c490beebc1"
+	}
